8d917db50bae6dbe91920c61aec69b7f2551b8c1
eliomarsantana <esantana.ti@gmail.com>
2016-04-03 09:07:55 -0300

    resolvendo atividade sobre dll

--- dll_and_shared_libraries/linux/dll.h
+++
@@ -1,7 +0,0 @@
-#ifdef BUILD_DLL
-#define MINHADLL __declspec(dllexport)
-#else
-#define MINHADLL __declspec(dllimport)
-#endif
-MINHADLL int soma(int a,int b);
-
--- dll_and_shared_libraries/windows/programa.c
+++ dll_and_shared_libraries/windows/programa.c
@@ -5,10 +5,13 @@
 #include "dll.h"
 
 typedef int (WINAPI * cfuncSoma)(int v1, int v2);
+typedef int (WINAPI * cfuncSubtracao)(int v1, int v2);
+typedef int (WINAPI * cfuncMultiplicacao)(int v1, int v2);
+typedef int (WINAPI * cfuncDivisao)(int v1, int v2);
 cfuncSoma FuncaoSoma;
-//cfuncSubtracao FuncaoSubtracao;
-//cfuncMultiplicacao FuncaoMultiplicacao;
-//cfuncDivisao FuncaoDivisao;
+cfuncSubtracao FuncaoSubtracao;
+cfuncMultiplicacao FuncaoMultiplicacao;
+cfuncDivisao FuncaoDivisao;
 
 
 int main(int argc,char **argv)
@@ -21,20 +24,20 @@ int main(int argc,char **argv)
     HINSTANCE hLib=LoadLibrary("minhadll.dll");
     if (!hLib) { printf("error"); return 0; }
     FuncaoSoma=(cfuncSoma)GetProcAddress((HMODULE)hLib, "soma");
-    //FuncaoSubtracao=(cfuncSubtracao)GetProcAddress((HMODULE)hLib, "subtracao");
-    //FuncaoMultiplicacao=(cfuncMultiplicacao)GetProcAddress((HMODULE)hLib, "multiplicacao");
-    //FuncaoDivisao=(cfuncDivisao)GetProcAddress((HMODULE)hLib, "divisao");
+    FuncaoSubtracao=(cfuncSubtracao)GetProcAddress((HMODULE)hLib, "subtracao");
+    FuncaoMultiplicacao=(cfuncMultiplicacao)GetProcAddress((HMODULE)hLib, "multiplicacao");
+    FuncaoDivisao=(cfuncDivisao)GetProcAddress((HMODULE)hLib, "divisao");
 
-    soma = FuncaoSoma(a, b);
-    //divisao = FuncaoSubtracao(a, b);
-    //multiplicacao = FuncaoMultiplicacao(a, b);
-    //divisao = FuncaoDivisao(a, b);
+    int soma = FuncaoSoma(a, b);
+    int subtracao = FuncaoSubtracao(a, b);
+    int multiplicacao = FuncaoMultiplicacao(a, b);
+    double divisao = FuncaoDivisao(a, b);
 
     FreeLibrary((HMODULE)hLib);
 
     printf("\nA soma de %d + %d = %d\n",a,b,soma);
-    //printf("\nA subtracao de %d + %d = %d\n",a,b,subtracao);
-    //printf("\nA multiplicacao de %d + %d = %d\n",a,b,multiplicacao);
-    //printf("\nA divisao de %d + %d = %f\n",a,b,divisao);
+    printf("\nA subtracao de %d - %d = %d\n",a,b,subtracao);
+    printf("\nA multiplicacao de %d * %d = %d\n",a,b,multiplicacao);
+    printf("\nA divisao de %d / %d = %f\n",a,b,divisao);
     system("pause");
 }
